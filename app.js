8a838ceee9ae7891d044247941edf8d513ec6d3a
wilsonobj <wilsonbritojunior@gmail.com>
2025-09-21 15:04:20 -0300

    atualizando java

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let amigos = [];
 
-// Função para adicionar um amigo à lista
 function adicionarAmigo() {
     let input = document.getElementById("amigo");
     let nome = input.value.trim();
@@ -10,21 +9,17 @@ function adicionarAmigo() {
         return;
     }
 
-    // Adiciona o nome no array
     amigos.push(nome);
 
-    // Atualiza a lista na tela
     atualizarLista();
 
-    // Limpa o campo de texto
     input.value = "";
     input.focus();
 }
 
-// Atualiza a lista de amigos na tela
 function atualizarLista() {
     let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = ""; // limpa antes de reescrever
+    lista.innerHTML = ""; 
 
     amigos.forEach((amigo) => {
         let li = document.createElement("li");
@@ -33,7 +28,6 @@ function atualizarLista() {
     });
 }
 
-// Função para sortear um amigo
 function sortearAmigo() {
     if (amigos.length === 0) {
         alert("Adicione pelo menos um nome antes de sortear!");
@@ -42,7 +36,6 @@ function sortearAmigo() {
 
     let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
 
-    // Exibir resultado
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = `<li>🎉 Amigo sorteado: <strong>${sorteado}</strong></li>`;
 }
